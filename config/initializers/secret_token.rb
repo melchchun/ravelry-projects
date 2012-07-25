d37aa315d0ce1c06fc715ff86566fa61ca199b01
melch <melch@snapizzi.com>
2012-07-24 22:41:04 -0700

    creating my knitting app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knitting::Application.config.secret_token = '042e3a0f95d24a6b3f0a522719dc43832bf3410d4e6d76c72a29af3a7f4441e7a30195ddcb2732af796be5bba2249e2d9dfcbe12121a850d1970a0ceaa7964d9'
